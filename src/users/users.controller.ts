8815e40c5ffdf6adeea1d5bf1d47f40bc744a96a
LoQeN00 <LoQeN00@o2.pl>
2022-10-24 18:03:29 +0200

    initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Controller, Post, Body, Get, Patch, Param, Query, Delete, UseInterceptors } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email.toLowerCase());
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+}
